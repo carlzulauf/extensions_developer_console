8fd4b739645943727d4022c843c01cc3cbed2840
Carl Zulauf <carl@linkleaf.com>
2013-08-11 17:42:04 -0500

    Add REPL interface to popup with simple object expansion

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,18 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   console.log("Loaded!")
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  $("#input-form").submit(function(e){
+    e.preventDefault();
+    result = eval( $("#input").val() );
+    if (Object.isObject( result )) {
+      tmp = "{\n"
+      Object.extended(result).each(function(key, value){
+        tmp = tmp + "  " + key + ": " + value + "\n"
+      })
+      result = tmp + "}"
+    }
+    if (Object.isArray( result )) {
+      result = "[" + result + "]"
+    }
+    $output = $("#output")
+    $output.text( result )
+  });
+});
