87dd068f1a20ea52b25dc678d4961cc425d5d283
Carl Zulauf <carl@linkleaf.com>
2013-08-12 10:26:58 -0500

    Small refactor and cleanup of REPL JS

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  var inspectObject = function(o, padding) {
+    var result = "{\n";
+    padding = padding || "  ";
+    Object.extended(o).each(function(key, value) {
+      result = result + padding + key + ": " + value + "\n";
+    });
+    return result + "}";
+  }
+
+  var inspectArray = function(a) {
+    return "[" + a + "]";
+  }
+
   $("#input-form").submit(function(e){
     e.preventDefault();
-    result = eval( $("#input").val() );
-    if (Object.isObject( result )) {
-      tmp = "{\n"
-      Object.extended(result).each(function(key, value){
-        tmp = tmp + "  " + key + ": " + value + "\n"
-      })
-      result = tmp + "}"
-    }
-    if (Object.isArray( result )) {
-      result = "[" + result + "]"
-    }
-    $output = $("#output")
-    $output.text( result )
+
+    var result = eval( $("#input").val() );
+
+    if (Object.isObject( result )) result = inspectObject( result );
+    if (Object.isArray(  result )) result = inspectArray(  result );
+
+    $("#output").text( result );
   });
 });
